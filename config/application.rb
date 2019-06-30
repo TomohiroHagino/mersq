7fb85370959f94f2b217367a2743e1d6e8b58f13
tomohiro hagino <marspeoplehg@gmail.com>
2019-06-30 20:16:18 +0900

    Static-pagesのコントローラーを追加

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Mer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Asia/Tokyo' #日時設定をアジア東京にセット
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
