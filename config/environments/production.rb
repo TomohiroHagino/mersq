ce321357cc8615a1c1737b3565c65f271d9d2921
tomohiro hagino <marspeoplehg@gmail.com>
2019-08-25 12:03:30 +0900

    ユーザーアカウント認証機能実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # emailサーバー設定をしたうえでこれをtrueにセットすると送信エラーを発生させることができます。
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app●●●●●●●>.herokuapp.com'
+  host = 'mersqishii.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
